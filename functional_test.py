2fa5612d36b2b68d225b60cdb2f9f2ecc340cd25
Gowri <gowri@gowri-VBox.(none)>
2014-04-10 08:32:14 -0700

    Refactor home page view to use a template

--- functional_test.py
+++ functional_test.py
@@ -1,8 +1,10 @@
-import unittest
-from selenium import webdriver
 
-class NewVisitorTest(TestCase):
-	def setUP(self):
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
 		self.browser = webdriver.Firefox()
 
 	def teardown(self):
@@ -17,18 +19,29 @@ class NewVisitorTest(TestCase):
 
 # She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('hl').text
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'), 
+			'Enter a to-do item'
+			)
 
 # She is invited to enter a to-do item straight away
 # She types "Buy peacock feathers" into a text box (Edith's hobby
 # is tying fly-fishing lures)
+		inputbox.send_keys('Buy peacock feathers')
 
 # When she hits enter, the page updates, and now the page lists
 # "1: Buy peacock feathers" as an item in a to-do list
+		inputbox.send_keys(Keys.ENTER)
 
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+			)
 # There is still a text box inviting her to add another item. She
 # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-
+		self.fail('Finish the test!')
 # The page updates again, and now shows both items on her list
 
 # Edith wonders whether the site will remember her list. Then she sees
--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,16 @@
+#this is our UNIT TEST
+from django.template.loader import render_to_string
 from django.test import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page
 
 # Create your tests here.
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		expected_html = render_to_string('home.html')
 	
-	def test_maths(self):
-		self.assertEqual(1+1, 3)
+		self.assertEqual(response.content.decode(), expected_html)
+
