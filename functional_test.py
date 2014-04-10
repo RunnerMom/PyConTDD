5e99308b1a800a97cc12d117986ee93203e328cc
Gowri <gowri@gowri-VBox.(none)>
2014-04-10 09:28:12 -0700

    Front page HTML now generated from a template

--- functional_test.py
+++ functional_test.py
@@ -3,13 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
-	def teardown(self):
+	def tearDown(self):
 		self.browser.quit()
-		self.browser.implicitly_wait(3)
+		#self.browser.implicitly_wait(3)
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 # Edith has heard about a cool new online to-do app. She goes
@@ -19,13 +20,16 @@ class NewVisitorTest(unittest.TestCase):
 
 # She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('hl').text
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+# She is invited to enter a to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'), 
 			'Enter a to-do item'
 			)
 
-# She is invited to enter a to-do item straight away
 # She types "Buy peacock feathers" into a text box (Edith's hobby
 # is tying fly-fishing lures)
 		inputbox.send_keys('Buy peacock feathers')
@@ -37,7 +41,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table"
 			)
 # There is still a text box inviting her to add another item. She
 # enters "Use peacock feathers to make a fly" (Edith is very methodical)
